92d2c339c0b43895c2d22fb7a37d277b3a792be0
Storm O'Bryant <vmarie1997@gmail.com>
2023-01-02 18:11:47 -0600

    need to deploy

--- CookieStandAdmin/ClientApp/src/newcomponents/Main.js
+++ CookieStandAdmin/ClientApp/src/newcomponents/Main.js
@@ -1,35 +1,62 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
+import axios from 'axios';
 
 
 export default function Main() {
+
+  const [location, setLocation] = useState(null);
+  const [min, setMin] = useState(null);
+  const [max, setMax] = useState(null);
+  const [average, setAverage] = useState(null);
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = "https://cookiestandapi-storm.azurewebsites.net/api/cookiestand";
+    // make axios request to api
+    const newStand = {
+      location,
+      MinimumCustomersPerHour : min,
+      MaximumCustomersPerHour : max,
+      AverageCookiesPerSale : average
+    }
+    console.log(newStand);
+    try {
+      console.log("before axios");
+      await axios.post(url, newStand)
+      console.log("after axios post");
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <>
-      <form style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gridTemplateRows: "repeat(3, 1fr)", columnGap: "1rem", rowGap: "1.5rem", backgroundColor: "#6FE6B7", padding: "1rem", borderRadius: "1%/5%" }}>
+      <form onSubmit={(e) => handleSubmit(e)} style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gridTemplateRows: "repeat(3, 1fr)", columnGap: "1rem", rowGap: "1.5rem", backgroundColor: "#6FE6B7", padding: "1rem", borderRadius: "1%/5%" }}>
         <div style={{ gridRow: "1/2", gridColumn: "1/5" }}>
           <h1 style={{ fontSize: "40px", textAlign: "center" }}>Create Cookie Stand</h1>
         </div>
 
         <div style={{ gridRow: "2/3", gridColumn: "1/5" }} class="form-group">
           <label for="location">Location</label>
-          <input type="text" class="form-control" id="location" placeholder="Enter location" />
+          <input onChange={(e) => setLocation(e.target.value)} type="text" class="form-control" id="location" placeholder="Enter location" />
         </div>
 
         <div style={{ gridRow: "3/4", gridColumn: "1/2" }} class="form-group">
           <label for="minimum_customers_per_hour">Minimum Customers Per Hour</label>
-          <input type="number" class="form-control" id="minimum_customers_per_hour" />
+          <input onChange={(e) => setMin(e.target.value)} type="number" class="form-control" id="minimum_customers_per_hour" />
         </div>
 
         <div style={{ gridRow: "3/4", gridColumn: "2/3" }} class="form-group">
           <label for="maximum_customers_per_hour">Maximum Customers Per Hour</label>
-          <input type="number" class="form-control" id="maximum_customers_per_hour" />
+          <input onChange={(e) => setMax(e.target.value)} type="number" class="form-control" id="maximum_customers_per_hour" />
         </div>
 
         <div style={{ gridRow: "3/4", gridColumn: "3/4" }} class="form-group">
           <label for="average_cookies_per_sale">Average Cookies Per Sale</label>
-          <input type="number" class="form-control" id="average_cookies_per_sale" />
+          <input onChange={(e) => setAverage(e.target.value)} type="number" class="form-control" id="average_cookies_per_sale" />
         </div>
 
-        <button type="submit" style={{ gridRow: "3/4", gridColumn: "4/5", backgroundColor: "#15B981", fontSize: "30px" }}>Create</button>
+        <button style={{ gridRow: "3/4", gridColumn: "4/5", backgroundColor: "#15B981", fontSize: "30px" }}>Create</button>
       </form>
     </>
   )
